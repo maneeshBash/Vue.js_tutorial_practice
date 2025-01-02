4b27d2df6f400e3b0b259c31bb9885ee569f914b
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-02 13:00:44 +0530

    pass data parent to child component

---
+++ src/components/ChildPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ name }}</h1>
+    <h2>name:{{ user.name }}  <br/>email: {{ user.email }}</h2>
+    <button v-on:click="getData()"> get data</button>
+
+</template>
+
+<script>
+    export default{
+        name:"ChildPage",
+        
+        props:{
+            name:String,
+            user:Object,
+            getData:Function
+        }
+    }
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>Hello from HomePage</h1>
+<Child name="Manish" :user="user" :getData="getData" />
+</template>
+
+<script>
+import Child from './ChildPage.vue'
+export default {
+    name: 'HomePage',
+    components: {
+        Child,
+    },
+    data(){
+        return{
+            user:{
+                name:"peter",
+                email:"peter@gmail.com"
+            }
+        }
+    },
+    methods:{
+        getData(){
+            console.warn("method called");
+            alert("function called")
+        }
+    }
+};
+</script>
