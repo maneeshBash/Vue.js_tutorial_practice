3449e5de65cb0b4af56fd740cf2a0f7513fed431
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-02 16:27:47 +0530

    HTML-tag binding

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,41 +1,19 @@
 <template>
 <h1>Hello from HomePage</h1>
-<ul>
-    <li v-for="item in users" :key="item.name">
-        <UserComp :data="item" :getData="getData" />
-
-    </li>
-</ul>
+<div v-html="text"></div>
+<div v-html="tag1"></div>
+<div v-html="tag2" ></div>
 </template>
 
 <script>
-import UserComp from './UserComp.vue'
 export default {
     name: 'HomePage',
-    components: {
-        UserComp,
-    },
-    methods: {
-        getData(name, email) {
-            alert(`Name: ${name}, Email:${email}`)
-        }
-    },
     data() {
         return {
-            users: [{
-                    name: "peter",
-                    email: "peter@gmail.com"
-                },
-                {
-                    name: "Joe",
-                    email: "Joe@gmail.com"
-                },
-                {
-                    name: "Jane",
-                    email: "Jane@gmail.com"
-                }
+            text: "Hello from Vue.js",
+            tag1: '<h1>h1 tag binding',
+            tag2: '<h2>h1 tag binding'
 
-            ]
         }
     }
 
