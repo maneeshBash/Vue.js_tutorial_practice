993e3e6dd96c3bb33eec38c06e0ec5715c9a6681
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-06 12:08:26 +0530

    add routes in our project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HomePage />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <nav>
+      <router-link to="/">Home</router-link>
+      <br/> <br />
+      <router-link to="/about">About</router-link>
+    </nav>
+    
+    <!-- This is where the routed components will be injected -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
-
+// You don't need to import HomeView and AboutView if you're routing them
 export default {
-  name: 'App',
-  components: {
-    HomePage,
-  }
+  name: 'App'
 }
 </script>
