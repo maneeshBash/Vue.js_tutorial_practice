b444ab614132368be08a510f000eed3c334f98cb
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-06 14:02:15 +0530

    Understanding and utilizing Vue's lifecycle hooks like created, mounted, beforeUnmount, unmounted

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
-  
-    <img alt="Vue logo" src="./assets/logo.png">
-    
-    <div>
-      <h1>{{ count }}</h1>
-      <button @click="increment">Increment</button>
-    </div>
+<img alt="Vue logo" src="./assets/logo.png">
+<br/>
+<input type="text" ref="myElement" />
 </template>
 
 <script>
-import {mapState} from 'vuex'
 export default {
-  name: 'App',
-  computed:{
-    ...mapState(['count'])
-  },
-  methods:{
-    increment(){
-      this.$store.commit('increment')
+    name: 'App',
+    data() {
+        return {
+            message: '',
+        }
+    },
+    created() {
+        this.message = 'Component has been created'
+        console.log(this.message)
+        alert(this.message)
+    },
+    mounted(){
+      console.warn('component is now mounted')
+      alert('component is now mounted')
+      this.$refs.myElement.focus();
+    },
+    beforeUnmount(){
+      console.warn("Component is about to be destroyed");
+      alert("Component is about to be destroyed")
+    },
+    unmounted(){
+      console.warn('Component has been destroyed');
+      alert('Component has been destroyed');
     }
-  }
+   
 }
 </script>
