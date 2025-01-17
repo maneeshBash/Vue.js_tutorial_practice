94b24193cd61c019a7e3c9f0239c6546a7a398ee
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-17 10:58:00 +0530

    give style for box-shadow

--- src/components/SlotChildPage.vue
+++ src/components/SlotChildPage.vue
@@ -25,7 +25,7 @@ export default {
     padding: 20px;
     margin: 15px;
     text-align: center;
-    box-shadow: 10px 15px 30px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
     background-color: #3498db;
     transition: all 0.5s ease, opacity 0.3s ease;
     opacity: 2;  
@@ -34,5 +34,4 @@ export default {
     transform: scale(1.1);
     opacity: 5;
 }
-
 </style>
