9ccb3c894837a71d39cac6c8768e02186af9c73d
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-24 12:36:46 +0530

    dynamic routing files: App.vue, index.js, main.js, ProfileView.vue, HomeView.vue, AboutView.vue,

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png"> <br>
 <router-link to="/">Home</router-link> <br>
-<router-link to="/about">About</router-link>
+<router-link to="/about">About</router-link> <br>
+<router-link to="/profile/hulk">Hulk's profile</router-link> <br>
+<router-link to="/profile/thor">Thor's profile</router-link> <br>
+<router-link to="/profile/ironMan">IronMan's profile</router-link> <br>
 
 <router-view></router-view>
 </template>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{profile}}'s ProfileView Page</h1>
+    </template>
+    
+    <script>
+    import { useRoute } from 'vue-router';
+        export default{
+            name:'ProfileView',
+            mounted(){
+                const route = useRoute();
+                console.warn("route", route.params.name)
+                this.profile = route.params.name
+            },
+            data(){
+                return{
+                    profile:'',
+                }
+            }
+        }
+    
+    </script>
