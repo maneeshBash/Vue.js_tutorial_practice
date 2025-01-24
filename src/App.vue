8251023ee8ab4a4b2f2d58e1f6636623c27f9694
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-24 11:57:41 +0530

    add file for static routing 1.App.vue, 2.main.js, 3.index.js, 4.AboutView.vue, 5.HomeView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,15 @@
 <template>
-<img alt="Vue logo" src="./assets/logo.png">
-<h1>beforeCreate and Created {{ name }}</h1>
+<img alt="Vue logo" src="./assets/logo.png"> <br>
+<router-link to="/">Home</router-link> <br>
+<router-link to="/about">About</router-link>
 
-<h1 ref="count">{{ counter }}</h1>
-<button v-on:click="counter=counter+1">Update Value</button>
-
-<button v-on:click="show=!show">Toggle button</button>
-<UnmountedComp  v-if="show"/>
+<router-view></router-view>
 </template>
 
 <script>
-import UnmountedComp from './components/UnmountedComp.vue';
 export default {
     name: 'App',
-    components:{
-        UnmountedComp
-    },
-    data() {
-        console.warn("created ")
-        return {
-            name: 'MSDhoni',
-            counter: 0,
-            show:true
-        }
-    },
-    beforeCreate() {
-        alert("beforeCrete")
-    },
-    created() {
-        alert("created")
-    },
-    beforeMount() {
-        console.warn("beforeMount", this.$el)
-    },
-    mounted() {
-        console.warn("mounted", this.$el)
-    },
-    beforeUpdate() {
-        console.warn("beforeUpdate", this.$refs['count'].textContent)
-    },
-    updated() {
-        console.warn("updated", this.$refs['count'].textContent)
-    }
+
 
 }
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>About Page</h1>
+<h1>AboutView Page</h1>
 </template>
 
 <script>
