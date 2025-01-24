3b7e48b9dda17458c2d423b5ea39fea10362b9bf
maneeshBash <manish.sonawane@onecelldx.com>
2025-01-24 10:53:55 +0530

    learn and implement all lifecycle methods

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png">
-<h1>Teleport Component</h1>
-<teleport to="#footer"><FooterPage/>
-</teleport>
+<h1>beforeCreate and Created {{ name }}</h1>
 
+<h1 ref="count">{{ counter }}</h1>
+<button v-on:click="counter=counter+1">Update Value</button>
+
+<button v-on:click="show=!show">Toggle button</button>
+<UnmountedComp  v-if="show"/>
 </template>
 
 <script>
-import FooterPage from './components/FooterPage.vue';
+import UnmountedComp from './components/UnmountedComp.vue';
 export default {
     name: 'App',
-  components:{
-    FooterPage
-  }
+    components:{
+        UnmountedComp
+    },
+    data() {
+        console.warn("created ")
+        return {
+            name: 'MSDhoni',
+            counter: 0,
+            show:true
+        }
+    },
+    beforeCreate() {
+        alert("beforeCrete")
+    },
+    created() {
+        alert("created")
+    },
+    beforeMount() {
+        console.warn("beforeMount", this.$el)
+    },
+    mounted() {
+        console.warn("mounted", this.$el)
+    },
+    beforeUpdate() {
+        console.warn("beforeUpdate", this.$refs['count'].textContent)
+    },
+    updated() {
+        console.warn("updated", this.$refs['count'].textContent)
+    }
+
 }
 </script>
---
+++ src/components/UnmountedComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 ref="user">In BeforeUnmount and Unmounted component</h1>
+</template>
+<script>
+export default{
+    name:'UnmountedComp',
+    beforeUnmount(){
+        console.log("beforeUnmount", this.$refs['user']);
+    },
+    unmounted(){
+        console.log("Unmounted", this.$refs['user']);
+    }
+
+}
+</script>
